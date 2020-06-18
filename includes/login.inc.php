7ad13654ff2a409a160a6cf18807f08056f3e59c
F471m4 <40557297+fatima-mo@users.noreply.github.com>
2020-06-18 02:38:55 -0700

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,47 +1,27 @@
 <?php
-
-if (isset($_POST['login-submit'])) {
-  require 'dbh.inc.php';
-
-  $mailuid = $_POST['mailuid'];
-  $password = $_POST['pwd'];
-
-  if (empty($mailuid) || empty($password)) {
-    header("Location: ../login.php?error=emptyfields");
-    exit();
-  }
-  else {
-    $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-
-      header("Location: ../login.php?error=sqlerror");
-      exit();
+require_once('dbh.inc.php');
+session_start();
+  if(isset($_POST['login-submit']))
+  {
+    if(empty($_POST['uid']) || empty($_POST['pwd']))
+    {
+      header("Location: ../login.php?Empty= Please fill in all fields.");
     }
-    else {
+    else
+    {
+      $query = "SELECT * FROM users WHERE uidUsers='".$_POST['uid']."' and pwdUsers='".$_POST['pwd']."'";
+      $result=mysqli_query($conn,$query);
 
-      mysqli_stmt_bind_param($stmt, "ss", $mailuid, $password);
-      mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_result($stmt);
-      if ($row = mysqli_fetch_assoc($result)) {
-        $pwdCheck = password_verify($password, $row['pwdUsers']);
-        if ($pwdCheck == false) {
-          header("Location: ../login.php?error=wrongpwd");
-          exit();
-        }
-        else if($pwdCheck == true)
-          session_start();
-          $_SESSION['userId'] =$row['idUsers'];
-          $_SESSION['userUid'] =$row['uidUsers'];
-
-          header("Location: ../status.php?login=success");
-          exit();
-        }
-        else { // wrong username and password
-          header("Location: ../login.php?error=wrongpwd");
-
-          exit();
-        }
+      if(mysqli_fetch_assoc($result))
+      {
+          $_SESSION['User']=$_POST['uid'];
+          header("Location: ../_success-login.php");
+      }
+      else
+      {
+        header("Location: ../login.php?Invalid= Wrong Username/Password.");
       }
     }
   }
+
+?>
